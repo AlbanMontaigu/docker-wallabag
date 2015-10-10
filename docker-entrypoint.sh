41918294db57cc8e65eb3950ad50c990afa10912
Alban Montaigu <alban.montaigu@gmail.com>
2015-10-10 14:07:03 +0200

    Try to fix installer enabled even if this is an upgrade

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,10 +20,22 @@ echo >&2 "[INFO] Complete! Backup successfully done in $(pwd)"
 echo >&2 "[INFO] ---------------------------------------------------------------"
 echo >&2 "[INFO] Installing or upgrading wallabag in $(pwd) - copying now..."
 echo >&2 "[INFO] ---------------------------------------------------------------"
-echo >&2 "[INFO] Updating / installing your installation with rsync"
+echo >&2 "[INFO] Checking if this is and upgrade"
+wb_upgrade=0
+if [ -f ./index.php ] 
+then
+    wb_upgrade=1
+fi
+echo >&2 "[INFO] wb_upgrade = $wb_upgrade"
+echo >&2 "[INFO] Syncing your installation with rsync and last source"
 rsync -urv /usr/src/wallabag/* .
 echo >&2 "[INFO] Removing cache"
 rm -rvf cache/*
+echo >&2 "[INFO] If upgrade, will remove the install directory (if not you won't see remove log)"
+if [ $wb_upgrade eq 1 ]
+then
+     rm -rvf ./install
+fi
 echo >&2 "[INFO] Fixing rights"
 chown -Rfv nginx:nginx .
 echo >&2 "[INFO] Complete! wallabag has been successfully installed / upgraded to $(pwd)"
